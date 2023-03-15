61f90188d3a0d3bbb16ea41f182a5916ee314325
EL-BOUHI HOUSSAM <houssam.el-bouhi@etu.unistra.fr>
2023-03-15 14:23:37 +0000

    Update nouveautetest.sql

--- Rendu_partie2_BDD_Houssam_Reda/nouveautetest.sql
+++ Rendu_partie2_BDD_Houssam_Reda/nouveautetest.sql
@@ -1,5 +1,5 @@
 insert into OBJET values (seq_objet.nextval,'80 jours dans le ciel','Livre','aventure');
 insert into OBJET values (seq_objet.nextval,'1899','série','aventure');
 
-select * from noouveute;
+select * from nouveaute;
 select * from nouveaute_objet;
